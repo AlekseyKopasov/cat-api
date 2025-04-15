12e0f40f0fc6c9b6d46e3a4efcda403219695cbd
Алексей Копасов <alesey.k1@gmail.com>
2025-04-15 12:12:18 +0300

    правит tsconfig.json

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { Form } from './components/Form/Form/Form';
-function App() {
-    return (_jsx("div", { className: 'flex flex-col gap-12 items-center justify-center max-w-[320px] min-h-screen m-auto', children: _jsx(Form, {}) }));
-}
-export default App;
